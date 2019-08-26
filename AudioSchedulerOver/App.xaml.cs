2ae21db50dde71c68c86f793288773dc24198333
Oleksandr Herhelezhyu <olek.h@outlook.com>
2019-08-26 23:25:12 +0300

    global logger added

--- AudioSchedulerOver/App.xaml.cs
+++ AudioSchedulerOver/App.xaml.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using AudioSchedulerOver.Logging;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Threading;
 
 namespace AudioSchedulerOver
 {
@@ -13,5 +15,17 @@ namespace AudioSchedulerOver
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            Logger.InitLogger();//инициализация - требуется один раз в начале
+            Logger.Log.Info(string.Format("Application Startup"));
+            Dispatcher.UnhandledException += OnDispatcherUnhandledException;
+        }
+
+        void OnDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            Logger.Log.Error(string.Format("Application exception {0} {1} {2}", e.Exception.Message, e.Exception.StackTrace, e.Exception.Data));
+            MessageBox.Show("Unhandled exception occurred: \n" + e.Exception.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
     }
 }
